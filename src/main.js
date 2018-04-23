348af2ad727b8845196f49d85d606432428e36db
Kelvin Omereshone <kelvinomereshone@gmail.com>
2018-04-23 08:57:29 -0700

    First installations

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
+import 'vue-material/dist/vue-material.min.css'
+import VueSweetalert2 from 'vue-sweetalert2'
 import App from './App'
 import router from './router'
-
 Vue.config.productionTip = false
 
+Vue.use(VueSweetAlert2)
+Vue.use(VueMaterial)
+Vue.use(Vuex)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
